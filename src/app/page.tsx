0c46837ec11f71f4584fa6d29a86195b55104aee
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-09-25 15:11:26 -0300

    add processo criativo e clientes section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+import Clientes from "./ui/home/clientes";
 import Etapas from "./ui/home/etapas";
 import Origem from "./ui/home/origem";
+import ProcessoCriativo from "./ui/home/processo-criativo";
 import Servicos from "./ui/home/servicos";
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center">
-      <Origem/>
+      <Origem />
       <div className='flex flex-col w-full items-center justify-center bg-blue-default'>
         <Servicos />
       </div>
-     <Etapas/>
+      <Etapas />
+      <div className='flex flex-col w-full items-center justify-center bg-blue-default'>
+        <ProcessoCriativo />
+      </div>
+      <div className='flex flex-col w-full items-center justify-center bg-[#f0f0f0]'>
+        <Clientes />
+      </div>
+     
     </div>
   );
 }
